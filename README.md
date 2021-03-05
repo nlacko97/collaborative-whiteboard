2e89133650107145b50f8e57f87252c962f146a8
Nagy Laszlo <nagylaszlolacko@gmail.com>
2021-03-05 13:50:06 +0200

    First commit: project setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# Collaborative whiteboard
+
+Real-time collaborative whiteboard application built with NodeJS and SocketIO.
+
+# Features
+
+# Roadmap
+
+# Contributions
+
+This app has been developed by
+
+## Installation
+- clone repository
+- run `npm install`
+- run `npm run start`
+- open browser and go to `localhost:3000`
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket) {
+    console.log("new client connected");
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('server listening on port ' + port));
